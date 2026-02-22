27ff0278fd220a1509c653eea718666f50eec9b4
m7amdabass1x22/design1 <M7amdabass1X22@gmail.com>
2026-02-22 02:50:39 +0200

    bala73

--- src/header.jsx
+++ src/header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
+import logo from "./assets/ChatGPT Image Feb 21, 2026, 02_32_14 AM.png";
 
 export default function Header() {
   return (
     <>
       <div className="w-full h-15 bg-black p-5 flex justify-center">
         <div className="container flex items-center justify-between">
-          <h1 className=" text-amber-50 font-bold text-[20px]">logo</h1>
-          <Nav/>
+<img src={logo} className="w-20" alt="" />          <Nav/>
         </div>
       </div>
     </>
